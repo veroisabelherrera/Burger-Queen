bb0f7dffa2e2140091f2b77875370f7b69d7dd49
GeryMolina <g.molina.ojeda@gmail.com>
2019-12-16 13:01:46 -0300

    Creacion de molecula segunda pantalla y de atomo input

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Organism from './components/organisms/Organism';
+
 class App extends Component {
   render(){
     return <div>
       <Organism />
+      
     </div>
   }
 }
--- src/Components/molecules/ButtonsContainer.js
+++ src/Components/molecules/ButtonsContainer.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react'
 import Button from '../atoms/Button';
+import H1 from '../atoms/H1';
+import Parraf from '../atoms/Parraf'
+import OrganismLogin from '../organisms/OrganismLogin'
 
 export default class ButtonsContainer extends Component {
+    
+    PassPage=()=>{
+
+        console.log("click");
+        return <OrganismLogin/>
+    }
+
     render() {
         
 
         const text = {
-            textoUno : 'mesero',
-            textoDos : 'cocina',
-            textoTres : 'cocinero',
-            textoCuatro : 'contraseña',
+            textoUno : 'Bienvenido!',
+            textoDos : 'Elige tu perfil para empezar',
+            textoTres : 'Mesero',
+            textoCuatro : 'Cocina',
         }
         return (
             <div>
-                
-                <Button title={text.textoUno}  style={btnOne}/>
-                <Button title={text.textoDos}/>
+                <H1 title={text.textoUno}/>
+                <Parraf title={text.textoDos}/>
+                <Button title={text.textoTres}  style={btnOne} click ={this.PassPage()}/>
+                <Button title={text.textoCuatro}/>
             </div>
         )
     }
