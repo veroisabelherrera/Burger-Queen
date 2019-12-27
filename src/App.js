0e342969ce49b78e5ae2662debbf0d115daf9074
Veronica Isabel <veronica@Veronica-Isabel.local>
2019-12-27 14:48:25 -0300

    crear cuenta, login y logout con firebase listo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-
 import './App.css';
-import Organism from './components/organisms/Organism';
+//import Organism from './components/organisms/Organism';
+import Auth from './Auth';
+import { useUser } from 'reactfire';
+
+/*
 class App extends Component {
   render(){
     return <div>
@@ -9,5 +12,21 @@ class App extends Component {
     </div>
   }
 }
+*/
+
+
+//Login con firebase - importando el componente de autentifiación
+
+function App() {
+  const user = useUser();
+
+  return(
+    <div className="App">
+      {user && <p>Bienvenido: { user.email } </p>}
+      <Auth />
+    </div>
+  );
+}
+
 
 export default App;
